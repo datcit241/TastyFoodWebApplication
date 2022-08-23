57a8b30e5d4de918368d352091a228f78f010b24
Dat Vo <dat.cit241@gmail.com>
2022-08-23 21:40:03 +0700

    Divide html templates into fragments

--- src/main/java/com/tastyfoodwebapplication/controllers/CartController.java
+++ src/main/java/com/tastyfoodwebapplication/controllers/CartController.java
@@ -1,7 +1,14 @@
 package com.tastyfoodwebapplication.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CartController {
+
+    @RequestMapping(value="/cart/add-product/{productId}")
+    public void addToCart(@PathVariable String productId) {
+
+    }
 }
